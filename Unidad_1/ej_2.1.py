503bcf779089ba7195836c2785748507bba67542
Alma Gámez <ajgamez02@gmail.com>
2023-10-02 18:30:29 -0600

    primerCOMMIT

---
+++ Unidad_1/ej_2.1.py
@@ -0,0 +1,7 @@
+#2.1 A la clase Array en el programa Array.py (Listado 2-3), agregue un método llamado getMaxNum() 
+#que devuelve el valor del más alto número en la matriz, o Ninguno si la matriz no tiene números.  
+#Puedes usar la expresión es instancia (x, (int, float)) para probar números. Agregue algo de código 
+#a ArrayClient.py (Listado 2-4) para ejercitar esto método.  Deberías probarlo en matrices que 
+#contengan una variedad de tipos de datos. y algunos que contienen ceros y otros que no contienen números.
+
+print("h")
