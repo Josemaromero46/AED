86cbe073169ce3abde92887cabbe7e35ae324d09
Alma Gámez <ajgamez02@gmail.com>
2023-10-10 22:03:30 -0600

    2-10/10COMMIT

--- Capitulo_4/4_2/ReverseWord.py
+++ Capitulo_4/4_2/ReverseWord.py
@@ -1,16 +1,16 @@
 
 from SimpleStack import *
 
-stack = Stack(100) # Crear una pila para contener las letras
-palabra = input("Palabra a invertir: ") #Pedir al usuario que ingrese una palabra para invertir
+stack = Stack(100)                           # Crear una pila para contener las letras
+palabra = input("Palabra a invertir: ")      # Pedir al usuario que ingrese una palabra para invertir
 
-for let in palabra: # Recorrer las letras de la palabra ingresada
-    if not stack.isFull(): # Agregar las letras a la pila si esta no está llena
+for let in palabra:                         # Recorrer las letras de la palabra ingresada
+    if not stack.isFull():                  # Agregar las letras a la pila si esta no está llena
         stack.push(let)
 
-revertir = '' # Construir la versión invertida de la palabra
+revertir = ''                                # Construir la versión invertida de la palabra
 
-while not stack.isEmpty(): # Al ir sacando elementos de la pila hasta que esté vacía
+while not stack.isEmpty():                   # Al ir sacando elementos de la pila hasta que esté vacía
     revertir += stack.pop()
 
 print('El inverso de ', palabra, 'es', revertir) #Imprimir la palabra original y su versión invertida
@@ -18,13 +18,13 @@ print('El inverso de ', palabra, 'es', revertir) #Imprimir la palabra original y
 
 clean_word = ''.join(e.lower() for e in palabra if e.isalpha()) #Eliminar caracteres no alfabéticos y convertir todo a minúsculas
 
-for let in clean_word: # Agregar las letras limpias a la pila
-    if not stack.isFull(): #Agregar letras a la pila si no está llena
+for let in clean_word:                               # Agregar las letras limpias a la pila
+    if not stack.isFull():                           #Agregar letras a la pila si no está llena
         stack.push(let)
 
-revertir = '' # "Construir la versión invertida"
+revertir = ''                                          # "Construir la versión invertida"
 
-while not stack.isEmpty(): # Al ir sacando elementos de la pila hasta que esté vacía
+while not stack.isEmpty():                            # Al ir sacando elementos de la pila hasta que esté vacía
     revertir += stack.pop()
 
 # Verificar si la palabra invertida es igual a la original
---
+++ DSAPCode/05_code/ex/5.1/LinkedList.py
@@ -0,0 +1,157 @@
+# Implement a singly linked list and a link class
+
+def identity(x): return x      # Identity function
+
+class Link(object):            # One datum in a linked list
+   def __init__(self, datum, next=None): # Constructor
+      self.__data = datum      # The datum for this link
+      self.__next = next       # Reference to next Link
+
+   def getData(self):          # Return the datum stored in this link
+      return self.__data
+
+   def setData(self, datum):   # Change the datum in this Link
+      self.__data = datum
+
+   def getNext(self): return self.__next # Return the next link
+
+   def setNext(self, link):    # Change the next link to a new Link
+      if link is None or isinstance(link, Link): #Must be Link or None
+         self.__next = link
+      else:
+         raise Exception("Next link must be Link or None")
+
+   def isLast(self):           # Test if link is last in the chain
+      return self.getNext() is None  # True if & only if no next Link
+
+   def __str__(self):          # Make a string representation of link
+      return str(self.getData())
+
+class LinkedList(object):      # A linked list of data elements
+   def __init__(self):         # Constructor
+      self.__first = None      # Reference to first Link
+
+   def getFirst(self): return self.__first # Return the first link
+
+   def setFirst(self, link):   # Change the first link to a new Link
+      if link is None or isinstance(link, Link): # It must be None or
+         self.__first = link   # a Link object
+      else:
+         raise Exception("First link must be Link or None")
+      
+   def getNext(self): return self.getFirst()    # First link is next
+   def setNext(self, link): self.setFirst(link) # First link is next
+
+   def isEmpty(self):          # Test for empty list
+      return self.getFirst() is None # True if & only if no 1st Link
+
+   def first(self):            # Return the first item in the list
+      if self.isEmpty():       # as long as it is not empty
+         raise Exception("No first item in empty list")
+      return self.getFirst().getData() # Return data item (not Link)
+   
+   def __iter__(self):         # Define an iterator for the list
+      next = self.getFirst()   # Start with first Link
+      while next is not None:   # As long as the link is not None,
+         yield next.getData()  # yield data for the link
+         next = next.getNext() # then move on to next link
+   
+   # def traverse(self,          # Apply a function to all items in list
+   #              func=print):   # with the default being to print
+   #    link = self.getFirst()   # Start with first link
+   #    while link is not None:  # Keep going until no more links
+   #       func(link.getData())  # Apply the function to the item
+   #       link = link.getNext() # Move on to next link
+
+   # def __len__(self):          # Get length of list
+   #    l = 0
+   #    link = self.getFirst()   # Start with first link
+   #    while link is not None:  # Keep going until no more links
+   #       l += 1                # Count Link in chain
+   #       link = link.getNext() # Move on to next link
+   #    return l
+   
+         
+   # def __str__(self):          # Build a string representation
+   #    result = "["             # Enclose list in square brackets
+   #    link = self.getFirst()   # Start with first link
+   #    while link is not None:  # Keep going until no more links
+   #       if len(result) > 1:   # After first link,
+   #          result += " > "    # separate links with right arrowhead
+   #       result += str(link)   # Append string version of link
+   #       link = link.getNext() # Move on to next link
+   #    return result + "]"      # Close with square bracket
+
+   def traverse(self, func=print):
+      for l in self:
+         func(l)
+
+   def __len__(self): 
+      count = 0
+      for l in self:
+         count+=1
+      return count
+
+      for i in range(0, 5): [0, 1, 2, 3, 4, 5 ]
+
+
+   def __str__(self):
+      result = "["
+      for l in self:
+         result += str(l) + " > "
+      return result[:-3] + "]" if len(result)>2 else result + "]"
+
+   def insert(self, datum):    # Insert a new datum at start of list
+      link = Link(datum,       # Make a new Link for the datum
+                  self.getFirst()) # What follows is the current list
+      self.setFirst(link)      # Update list to include new Link
+
+   def find(                   # Find the 1st Link whose key matches
+         self, goal, key=identity): # the goal
+      link = self.getFirst()   # Start at first link
+      while link is not None:  # Search until the end of the list
+         if key(link.getData()) == goal:  # Does this Link match?
+            return link        # If so, return the Link itself
+         link = link.getNext() # Else, continue on along list
+         
+   def search(                 # Find 1st item whose key matches goal
+         self, goal, key=identity):
+      link = self.find(goal, key) # Look for Link object that matches
+      if link is not None:     # If found,
+         return link.getData() # return its datum
+
+   def insertAfter(            # Insert a new datum after the first
+         self, goal, newDatum, # Link with a matching key
+         key=identity):
+      link = self.find(goal, key)  # Find matching Link object
+      if link is None:         # If not found,
+         return False          # return failure
+      newLink = Link(          # Else build a new Link node with
+         newDatum, link.getNext()) # new datum and remainder of list
+      link.setNext(newLink)    # and insert after matching link
+      return True
+
+   def deleteFirst(self):      # Delete first Link
+      if self.isEmpty():       # Empty list? Raise an exception
+         raise Exception("Cannot delete first of empty list")
+      
+      first = self.getFirst()  # Store first Link
+      self.setFirst(first.getNext()) # Remove first link from list
+      return first.getData()   # Return first Link's data
+
+   def delete(self, goal,      # Delete the first Link from the
+              key=identity):   # list whose key matches the goal
+      if self.isEmpty():       # Empty list? Raise an exception
+         raise Exception("Cannot delete from empty linked list")
+
+      previous = self          # Link or LinkedList before Link
+      while previous.getNext() is not None: # to be deleted
+         link = previous.getNext()  # Next link after previous
+         if goal == key(link.getData()): # If next Link matches,
+            previous.setNext(  # change the previous' next
+               link.getNext()) # to be Link's next and return
+            return link.getData() # data since match was found
+         previous = link       # Advance previous to next Link
+         
+      # Since loop ended without finding item, raise exception
+      raise Exception("No item with matching key found in list")
