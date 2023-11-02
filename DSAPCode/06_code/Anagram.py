37e11b97c0d1eead0f326e5597eaa3a5fbf59b54
Alma Gámez <ajgamez02@gmail.com>
2023-11-02 14:30:09 -0600

    Commit_2/11/2023

--- DSAPCode/06_code/Anagram.py
+++ DSAPCode/06_code/Anagram.py
@@ -1,13 +1,22 @@
-# Compute the anagrams of a string of characters
+# Calcular los anagramas de una cadena de caracteres
 
-def anagrams(word):            # Return a list of anagrams for word
-   if len(word) <= 1:          # Empty words and single letters
-      return [word]            # have a single anagram, themselves
-   result = []                 # Start with an empty list
-   for part in anagrams(word[1:]): # Loop over smaller anagrams
-      for i in range(len(part) + 1): # For each index in smaller word
-         result.append(        # Add a new anagram with
-            part[:i] +         # the smaller word up to the index
-            word[0] +          # plus the 1st character of this word
-            part[i:])          # plus the rest of the smaller word
-   return result               # Return the list of bigger anagrams
+def factorial(n):         # Obtener el factorial de n
+   if n < 1: return 1     # Es 1 para cualquier número < 1
+   return (n *            # De lo contrario, multiplica n
+   factorial(n - 1))      # por el factorial anterior
+
+def anagramas(palabra):                 # Devuelve una lista de anagramas para la palabra
+   if len(palabra) <= 1:                # Palabras vacías y letras individuales
+      return [palabra]                  # tienen un solo anagrama, ellos mismos
+   resultado = []                       # Comenzar con una lista vacía
+   for parte in anagramas(palabra[1:]): # Iterar sobre anagramas más pequeños
+      for i in range(len(parte) + 1):   # Para cada índice en la palabra más pequeña
+         resultado.append(              # Agregar un nuevo anagrama con
+            parte[:i] +                 # la palabra más pequeña hasta el índice
+            palabra[0] +                # más el primer carácter de esta palabra
+            parte[i:])                  # más el resto de la palabra más pequeña
+   return resultado                     # Devolver la lista de anagramas más grandes
+
+word = input("\nIngrese una palabra: ")
+print(f"\nCantidad de Anagramas de {word}: {factorial(len(word))}")
+print(f"\nAnagramas de {word}: {anagramas(word)}")
